cbee4f7b709af2b7b2334ba43fe7f5fae8f58803
aganezov <aganezov@cs.jhu.edu>
2020-07-07 16:08:56 -0400

    test to ensure that alignment object creation auto-check the correctness of the supplied cigar string

--- test/test_alignment.py
+++ test/test_alignment.py
@@ -1,4 +1,6 @@
 import pytest
+from hypothesis import given
+import hypothesis.strategies as st
 
 from cigarco.mapping import Alignment
 
@@ -8,15 +10,23 @@ def test_empty_alignment_creation():
         Alignment()
 
 
-def test_basic_alignment_attributes():
-    alignment = Alignment("tr1", "chr1", 0, "11M")
-    assert alignment.query_name == "tr1"
-    assert alignment.target_name == "chr1"
-    assert alignment.start == 0
-    assert alignment.cigar == "11M"
+@given(query=st.text(), target=st.text(), start=st.integers(min_value=0))
+def test_basic_alignment_attributes(query, target, start):
+    cigar = "10M"   # manual valid cigar to ensure we pass the validity check
+    alignment = Alignment(query, target, start, cigar)
+    assert alignment.query_name == query
+    assert alignment.target_name == target
+    assert alignment.start == start
+    assert alignment.cigar == cigar
 
 
-def test_invalid_start_in_alignment():
+@given(start=st.integers(max_value=-1))
+def test_invalid_start_in_alignment(start):
     with pytest.raises(ValueError):
-        Alignment("tr1", "chr1", -1, "11M")
+        Alignment("tr1", "chr1", start, "11M")
+
+
+def test_invalid_cigar_argument():
+    with pytest.raises(ValueError):
+        Alignment("tr1", "chr1", 0, "10")
 
