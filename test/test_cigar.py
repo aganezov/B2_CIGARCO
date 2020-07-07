e35a7adc80776bbf5d7a9ab44a74c3a7024e1bc9
aganezov <aganezov@cs.jhu.edu>
2020-07-07 14:38:16 -0400

    test to cover cases of invalid cigar string

--- test/test_cigar.py
+++ test/test_cigar.py
@@ -13,7 +13,24 @@ def test_invalid_cigar_consecutive_letters():
     assert is_valid_cigar("11MM")
 
 
-@pytest.mark.xfail(reason="CIGAR string does nto allow for consecutive letters")
+@pytest.mark.xfail(reason="CIGAR string does not allow for consecutive letters")
 def test_invalid_cigar_no_letters():
     assert is_valid_cigar("11")
+
+
+@pytest.mark.xfail(reason="CIGAR string must have a number encoding the amount of time a subsequently specified operation is encoded")
+def test_invalid_cigar_no_starting_number():
+    assert is_valid_cigar("M")
+
+
+@pytest.mark.xfail(reason="CIGAR string must have an operation encoded  as a single letter after every provided number")
+def test_invalid_cigar_no_ending_operation():
+    assert is_valid_cigar("11M10")
+
+
+@pytest.mark.xfail(reason="CIGAR string only supports MIDNSHP=X operation encoding characters")
+def test_invalid_cigar_unsupported_operation_character():
+    assert is_valid_cigar("10A11M")
+
+
 
