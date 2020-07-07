ef601155e8c8c9760ccefb5e169b8e67772ebbdb
aganezov <aganezov@cs.jhu.edu>
2020-07-07 17:09:33 -0400

    initial setup for the CMapper class designed for performing coordinate (re)mapping between query and target coordinate systems based on the CIGAR string alignment description

--- cigarco/mapping.py
+++ cigarco/mapping.py
@@ -32,3 +32,25 @@ class Alignment(object):
             raise ValueError(f"incorrect start coordinate {self.start}. Must be a non-negative integer")
         if not is_valid_cigar(self.cigar):
             raise ValueError(f"invalid CIGAR string '{self.cigar}'")
+
+
+@dataclass
+class CMapper(object):
+    """
+    The main object that provides coordinate conversion for a given Alignment object.
+    The main computational idea behind the CMapper machinery is the prefix sum arrays, which are used to efficiently convert the query coordinate to the target coordinate
+
+    Several optimization techniques are utilized to speed up the process:
+        * prefix sum arrays are computed only once on first access via a combination of private attributes and property access control
+        * alignment object reassignment (controlled via property setter) invalidates the computed prefix sums ensuring that coordinate mapping is always done
+            w.r.t. current alignment; Note: changes in alignment object are nto tracked, though this can be implemented in the future via a descriptor-protocol on alignment
+        * cashing is utilized for mapping function
+
+    Overall complexity of the CMapper is dependent on the size n of the CIGAR alignment string (where n refers to the number of operations in a parsed CIGAR string):
+        * prefix sum construction takes O(n); on first access only
+        * coordinate mapping takes O(log(n)) steps at worst (and O(1) for repeated lookups)
+
+    Args:
+        alignment (Alignments): an alignment object for which the coordinate conversion (i.e., mapping) is performed. Alignment object is assumed to be valid
+    """
+    alignment: Alignment
