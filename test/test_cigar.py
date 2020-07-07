b5d2354bf095bcd4b4b11f989608d611a4202a0d
aganezov <aganezov@cs.jhu.edu>
2020-07-07 12:30:14 -0400

    basic attribute organization test

--- test/test_cigar.py
+++ test/test_cigar.py
@@ -6,3 +6,11 @@ from cigarco.mapping import Alignment
 def test_empty_alignment_creation():
     with pytest.raises(TypeError):
         Alignment()
+
+
+def test_basic_alignment_attributes():
+    alignment = Alignment("tr1", "chr1", 0, "11M")
+    assert alignment.query_name == "tr1"
+    assert alignment.target_name == "chr1"
+    assert alignment.start == 0
+    assert alignment.cigar == "11M"
