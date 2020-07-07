45cf0cbed035d8351f35c57bf79bb8fb2a02955a
aganezov <aganezov@cs.jhu.edu>
2020-07-07 14:55:37 -0400

    adding hypothesis testing framework into the mix for a more robust testing setup

--- test/test_cigar.py
+++ test/test_cigar.py
@@ -1,36 +1,39 @@
-import pytest
+from hypothesis import given
+from hypothesis.strategies import text, integers
+import string
 
-from cigarco.cigar_utils import is_valid_cigar
+from cigarco.cigar_utils import is_valid_cigar, ALLOWED_OPERATIONS
 
 
-@pytest.mark.xfail(reason="empty CIGAR string is not valid")
 def test_empty_cigar_validity():
-    assert is_valid_cigar("")
+    assert not is_valid_cigar("")
 
 
-@pytest.mark.xfail(reason="CIGAR string does nto allow for consecutive letters")
-def test_invalid_cigar_consecutive_letters():
-    assert is_valid_cigar("11MM")
+@given(number=integers(min_value=0), operation=text(alphabet=ALLOWED_OPERATIONS, min_size=2, max_size=100))
+def test_invalid_cigar_consecutive_letters(number, operation):
+    assert not is_valid_cigar(f"{number}{operation}")
 
 
-@pytest.mark.xfail(reason="CIGAR string does not allow for consecutive letters")
-def test_invalid_cigar_no_letters():
-    assert is_valid_cigar("11")
+@given(number=integers(min_value=0))
+def test_invalid_cigar_no_letters(number):
+    assert not is_valid_cigar(f"{number}")
 
 
-@pytest.mark.xfail(reason="CIGAR string must have a number encoding the amount of time a subsequently specified operation is encoded")
-def test_invalid_cigar_no_starting_number():
-    assert is_valid_cigar("M")
+@given(operation=text(alphabet=ALLOWED_OPERATIONS, min_size=1, max_size=1))
+def test_invalid_cigar_no_starting_number(operation):
+    assert not is_valid_cigar(f"{operation}")
 
 
-@pytest.mark.xfail(reason="CIGAR string must have an operation encoded  as a single letter after every provided number")
-def test_invalid_cigar_no_ending_operation():
-    assert is_valid_cigar("11M10")
+@given(n1=integers(min_value=0),
+       operation=text(alphabet=ALLOWED_OPERATIONS, min_size=1, max_size=1),
+       n2=integers(min_value=0))
+def test_invalid_cigar_no_ending_operation(n1, operation, n2):
+    assert not is_valid_cigar(f"{n1}{operation}{n2}")
 
 
-@pytest.mark.xfail(reason="CIGAR string only supports MIDNSHP=X operation encoding characters")
-def test_invalid_cigar_unsupported_operation_character():
-    assert is_valid_cigar("10A11M")
-
-
-
+@given(n1=integers(min_value=0),
+       operation=text(alphabet=ALLOWED_OPERATIONS, min_size=1, max_size=1),
+       n2=integers(min_value=0),
+       unsupported_operation=text(alphabet=set(string.ascii_uppercase) - ALLOWED_OPERATIONS, min_size=1, max_size=1))
+def test_invalid_cigar_unsupported_operation_character(n1, operation, n2, unsupported_operation):
+    assert not is_valid_cigar(f"{n1}{operation}{n2}{unsupported_operation}")
