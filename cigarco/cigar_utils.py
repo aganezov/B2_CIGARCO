2fc81ce12a6bef427ab2fd2305e51a066cd9ce75
aganezov <aganezov@cs.jhu.edu>
2020-07-07 14:38:44 -0400

    initial commit with tarting implementation of CIGAR string manipulation utils

---
+++ cigarco/cigar_utils.py
@@ -0,0 +1,36 @@
+ALLOWED_OPERATION = {'M', "I", "D", "N", "S", "H", "P", "=", "X"}
+QUERY_CONSUMING_OPERATIONS = ALLOWED_OPERATION - {"D", "N", "H", "P"}
+TARGET_CONSUMING_OPERATIONS = ALLOWED_OPERATION - {"I", "S", "H", "P"}
+
+
+def is_valid_cigar(cigar: str) -> bool:
+    """
+    A CIGAR string is valid if it is non-empty string with alternating numbers and supporting single character operations
+
+    Args:
+        cigar (str): a CIGAR encoded (doc: https://samtools.github.io/hts-specs/SAMv1.pdf , page 7) alignment string
+
+    Returns:
+        valid (bool): a flag indicating if a cigar string is valid one.
+    """
+    # can't be an empty one
+    if not cigar:
+        return False
+    prev = None
+    for char in cigar:
+
+        # Every entry has to be either a digit or a supporter single-char encoded operation
+        if not (char.isdigit() or char in ALLOWED_OPERATION):
+            return False
+
+        # CIGAR string must start with a digit
+        if char in ALLOWED_OPERATION and (not prev or not prev.isdigit()):
+            return False
+
+        prev = char
+
+    # CIGAR string must end with an operation
+    if prev not in ALLOWED_OPERATION:
+        return False
+
+    return True
