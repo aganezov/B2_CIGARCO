811583d87bdd70c972991fe6d95c66293afdaaea
aganezov <aganezov@cs.jhu.edu>
2020-07-11 13:05:33 -0400

    initial addition of the setup.py script for potential distribution on PyPi as well as for integration testing in CI environment

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+import sys
+import os
+
+import cigarco
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+setup(
+    name="CIGARCO",
+    version=cigarco.version,
+    author="Sergey Aganezov",
+    author_email="sergeyaganezovjr@gmail.com",
+    description="A tool for computationally efficient transformation of coordinates between query and target aligned sequences",
+    license="MIT",
+    keywords="Bioinformatics, CIGAR, alignments, coordinate transformation",
+    url="https://github.com/aganezov/rck",
+    packages=["", "cigarco"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "cogarco = cigarco.app:main",
+        ]
+    },
+    install_requires=["pytest", "hypothesis"]
+)
